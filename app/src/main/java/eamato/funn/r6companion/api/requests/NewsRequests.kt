a091d4a969fba9b39482f814e87bd3633b588e1c
eamato <veaceslav.buzdugan@gmail.com>
2019-11-03 15:17:44 +0200

    fast change

--- app/src/main/java/eamato/funn/r6companion/api/requests/NewsRequests.kt
+++ app/src/main/java/eamato/funn/r6companion/api/requests/NewsRequests.kt
@@ -1,5 +1,6 @@
 package eamato.funn.r6companion.api.requests
 
+import eamato.funn.r6companion.api.ApiClient
 import eamato.funn.r6companion.utils.API_KEY_PARAM_KEY
 import eamato.funn.r6companion.utils.API_KEY_PARAM_VALUE
 import eamato.funn.r6companion.utils.NEWS_HOST
@@ -11,7 +12,7 @@ import retrofit2.http.Query
 interface NewsRequests {
 
     companion object {
-        val HOST = NEWS_HOST
+        fun getNewsRequest() = ApiClient.getApiClient(NEWS_HOST).create(NewsRequests::class.java)
     }
 
     @GET(SOURCES_PATH)
