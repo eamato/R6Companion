2a885ce1d531762e5f570b9537e1db3c90c263f6
eamato <veaceslav.buzdugan@gmail.com>
2019-11-16 21:14:53 +0200

    new design

--- app/src/main/java/eamato/funn/r6companion/ui/fragments/PacketOpeningFragment.kt
+++ app/src/main/java/eamato/funn/r6companion/ui/fragments/PacketOpeningFragment.kt
@@ -31,7 +31,7 @@ class PacketOpeningFragment : BaseFragment(), SurfaceHolder.Callback {
 
     private val backgroundColor: Int by lazy {
         context?.let { nonNullContext ->
-            ContextCompat.getColor(nonNullContext, R.color.colorOpenPackBackground)
+            ContextCompat.getColor(nonNullContext, R.color.colorBackground)
         } ?: throw Exception("Invalid context")
     }
 
